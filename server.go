29dfb798e3e06f386d05936c9488691bfdd31b81
Karandeep Singh <karandeep070104@gmail.com>
2024-09-07 15:09:37 +0530

    new-endpoint

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/hello-world", handleHelloWorld)
 	http.HandleFunc("/health", handleHealth)
-
+	http.HandleFunc("/new-endpoint", handleEndPoint)
 	addr := "localhost:8000"
 	log.Printf("Listening on %s ...", addr)
 
@@ -20,6 +20,12 @@ func main() {
 	}
 }
 
+func handleEndPoint(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	writeResponse(writer, "new EndPoint")
+}
 func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != "GET" {
 		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
